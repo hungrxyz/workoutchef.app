eb7000d988caa7834ab17e360c7b0ba838b7b8f3
hungrxyz <6236080+hungrxyz@users.noreply.github.com>
2025-08-14 22:59:36 +0200

    more styling, color match n stuff

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -5,15 +5,8 @@ struct Home: StaticPage {
     var title = "Home"
 
     var body: some HTML {
-        VStack(spacing: 50) {
-            Text("WorkoutChef")
-                .font(.title1)
-                .fontWeight(.heavy)
-            
-            Text("Build workouts that fit you.")
-                .font(.title4)
-            
-            Text("Send them straight to your Apple Watch and train without thinking about what’s next.")
+        VStack(spacing: 20) {
+            Spacer(size: 25)
             
             Image("/images/AppIcon.png")
                 .aspectRatio(1, contentMode: .fit)
@@ -21,30 +14,44 @@ struct Home: StaticPage {
                 .cornerRadius(56)
                 .clipped()
             
+            Text("WorkoutChef")
+                .font(.title1)
+                .fontWeight(.black)
+                .horizontalAlignment(.center)
+            
+            Text("Build workouts that fit you.")
+                .font(.title6)
+                .horizontalAlignment(.center)
+            
+            Text("Send them straight to your Apple Watch and train without thinking about what’s next.")
+                .foregroundStyle(.secondary)
+                .horizontalAlignment(.center)
+            
             Form {
-                TextField("Email", prompt: "john@doe.com")
+                TextField("Email", prompt: "Email")
                     .type(.email)
                     .customAttribute(name: "name", value: "email")
                     .required()
+                    .class("col-md-12")
                 
-                TextField("Purpose", prompt: "What’s your main activity?")
+                TextField("Purpose", prompt: "What type of workouts do you do? e.g. Run, HIIT, Strength Training,...")
                     .type(.text)
                     .customAttribute(name: "name", value: "activity")
+                    .class("col-md-12")
                 
                 Button("Join Waitlist")
                     .role(.primary)
                     .type(.submit)
+                    .class("col-md-12")
+                    .style(.backgroundColor, "#A5DB61")
+                    .style(.color, Color.black.description)
+                    .style(.borderColor, "#A5DB61")
+                    .style(.fontWeight, "bold")
             }
             .labelStyle(.hidden)
             .class("launchlist-form")
             .attribute("action", "https://getlaunchlist.com/s/u64q47")
             .attribute("method", "POST")
-            
-            Script(code: "")
-                .attribute("src", "https://getlaunchlist.com/js/widget-diy.js")
-                .attribute("defer")
-            
-            Spacer(size: 200)
         }
     }
 }
